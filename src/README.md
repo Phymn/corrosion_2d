fae2cf46124ff60cf8d23afc9badca0ec9cbac09
Hinken <phnome@protonmail.com>
2023-01-22 21:14:02 +0100

    adding readme

---
+++ src/README.md
@@ -0,0 +1,9 @@
+Corrosion2D is a game engine written in Rust.
+
+> Corrosion2D is not meant to be a competitor to other engines written in Rust.
+> The idea behind this is to learn more about game development and Rust.
+
+The idea spawned from simple need, and felt that WGPU and such was a bit too much.
+Use Bevy or something based on WGPU if you want to support all the fancy stuffs.
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+extern crate raylib;
+use raylib::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let width = 800;
+    let height = 600;
+    let (mut rl, thread) = raylib::init()
+        .size(width, height).title("Corrosion2D").build();
+
+    rl.set_target_fps(60);
+
+    while !rl.window_should_close() {
+        let pressed_keys = rl.get_key_pressed();
+        let mut d = rl.begin_drawing(&thread);
+
+        d.clear_background(Color::WHITE);
+
+        if let Some(pressed_key) = pressed_keys {
+            d.draw_text(&format!("{:?}", pressed_key), 100, 12, 10, Color::BLACK);
+        }
+        //println!("{:?}", pressed_key);
+    }
 }
